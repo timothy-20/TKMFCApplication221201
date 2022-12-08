963027bdf72e0b23cfa65f268bbef71232ff220b
timothy-20 <jungun3329@gmail.com>
2022-12-08 16:43:33 +0900

    221208) - iterator 구현 중.

--- TKMFCConsoleApplication221201/TKMFCConsoleApplication221201.cpp
+++ TKMFCConsoleApplication221201/TKMFCConsoleApplication221201.cpp
@@ -1,27 +1,71 @@
-﻿#include <stdio.h>
+﻿#define _CTRDBG_MAP_ALLOC 
+#include <stdio.h>
+#include <stdlib.h>
 #include <iostream>
+#include <crtdbg.h>
 
-inline void IncreaseNumber(int*& pl)
+#ifdef _DEBUG
+#define new new(_NORMAL_BLOCK, __FILE__, __LINE__)
+#endif // _DEBUG
+
+
+//enum TKErrorType{
+//	TKErrorNullPointer = 0x00,
+//	TKErrorOutOfRange,
+//} ;
+
+template<typename TYPE, size_t LENGTH>
+class TKDummyList
 {
-	if (pl == nullptr)
+private:
+	TYPE m_list[LENGTH];
+
+public:
+	struct Iterator
 	{
-		int defalutValue = 0;
-		pl = &defalutValue;
-	}
-	else
-	{
-		*pl += 10;
-	}
-}
+		using IteratorCategory = std::forward_iterator_tag;
+		using DifferenceType = std::ptrdiff_t;
+		using GenericType = TYPE;
+		using PointerType = TYPE*;
+		using ReferenceType = TYPE&;
+
+	private:
+		PointerType m_ptr;
+
+	public:
+		Iterator(PointerType ptr) : m_ptr(ptr) {}
+		//접두사에 붙은 증감연산자
+		Iterator& operator++() 
+		{ 
+			this->m_ptr++; 
+			return *this;
+		}
+		//접미사에 붙은 증감연산자
+		Iterator operator++(int)
+		{
+			Iterator weakThis = *this;
+			++(*this);
+			return weakThis;
+		}
+		ReferenceType operator*() const { return *this->m_ptr; }
+		PointerType operator->() { return this->m_ptr; }
+		friend bool operator==(const Iterator& a, const Iterator& b) { return a.m_ptr == b.m_ptr; }
+		friend bool operator!=(const Iterator& a, const Iterator& b) { return a.m_ptr != b.m_ptr; }
+	};
+
+	Iterator begin() {return Iterator(&this->m_list) }
+};
 
 int main()
 {
-	int* ptrValue = nullptr;
+	int* npArray[10] = {};
+	std::fill(std::begin(npArray), std::end(npArray), new int(1));
 
-	::IncreaseNumber(ptrValue);
-	std::cout << "result: " << *ptrValue << std::endl;
-	::IncreaseNumber(ptrValue);
-	std::cout << "result: " << *ptrValue << std::endl;
+	for (const int* element : npArray)
+	{
+		std::cout << "element: " << element << std::endl;
+	}
 
+	_CrtDumpMemoryLeaks();
 	return 0;
 }
