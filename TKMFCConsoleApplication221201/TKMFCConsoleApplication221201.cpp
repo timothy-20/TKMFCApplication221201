a9e2775986df84784d3e84fd417eddabae6f2872
timothy-20 <jungun3329@gmail.com>
2022-12-08 18:14:43 +0900

    221208) - custom iterator 테스트 중.

--- TKMFCConsoleApplication221201/TKMFCConsoleApplication221201.cpp
+++ TKMFCConsoleApplication221201/TKMFCConsoleApplication221201.cpp
@@ -4,6 +4,8 @@
 #include <iostream>
 #include <crtdbg.h>
 
+#include <vector>
+
 #ifdef _DEBUG
 #define new new(_NORMAL_BLOCK, __FILE__, __LINE__)
 #endif // _DEBUG
@@ -53,18 +55,42 @@ public:
 		friend bool operator!=(const Iterator& a, const Iterator& b) { return a.m_ptr != b.m_ptr; }
 	};
 
-	Iterator begin() {return Iterator(&this->m_list) }
+	Iterator begin() { return Iterator(&this->m_list[0]); }
+	Iterator end() { return Iterator(&this->m_list[LENGTH]); }
 };
 
 int main()
 {
-	int* npArray[10] = {};
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	std::cout.tie(nullptr);
+
+	/*int* npArray[10] = {};
 	std::fill(std::begin(npArray), std::end(npArray), new int(1));
 
 	for (const int* element : npArray)
 	{
 		std::cout << "element: " << element << std::endl;
-	}
+	}*/
+
+	TKDummyList<int, 10> dummyList;
+
+	std::fill(dummyList.begin(), dummyList.end(), 10);
+
+	for (auto element : dummyList)
+		std::cout << "1. element: " << element << std::endl;
+
+	std::cout << std::endl;
+
+	for (auto begin = dummyList.begin(), end = dummyList.end(); begin != end; begin++)
+		std::cout << "2. element: " << *begin << std::endl;
+
+	std::string text = std::string("abcde");
+	const char* appendChar = "EEEE";
+	
+	text.append(appendChar);
+
+	const char* cText = text.c_str();
 
 	_CrtDumpMemoryLeaks();
 	return 0;
